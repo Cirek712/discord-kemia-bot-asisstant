aa6c1ee0540a8142ce64fe89f6a86c198a2cf393
Cirek712 <enzorebolle13@gmail.com>
2022-10-24 16:23:19 +0200

    Release
    Release

---
+++ Commands.cs
@@ -0,0 +1,18 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.Interactions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+public class Commands : InteractionModuleBase<SocketInteractionContext>
+{
+    [SlashCommand("say", "Envoi un message. ⚠️ Commande reservée aux modérateurs.", false, Discord.Interactions.RunMode.Async)]
+    public async Task Say(string input)
+    {
+        await Context.Channel.SendMessageAsync(input);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,82 @@
+﻿using Discord.Net;
+using Discord;
+using Discord.WebSocket;
+using Discord.Interactions;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+public class Program
+{
+    public const string TOKEN = "MTAzNDA4Njk3OTA2OTAzMDQzMA.G41B8h.m9X7A8XGMwwTrePjFfJCLmrAu_CRT0XqdNEWVc";
+    private DiscordSocketClient theClient;
+    private IServiceProvider _serviceProvider;
+
+    static Task Main() => new Program().MainAsync();
+
+    static IServiceProvider CreateProvider()
+    {
+        var collection = new ServiceCollection();
+        //...
+        return collection.BuildServiceProvider();
+    }
+
+    public async Task MainAsync()
+    {
+        theClient = new();
+
+        _serviceProvider = CreateProvider();
+
+        // ---------------
+
+        // Run Seq.1
+        Console.WriteLine("Starting Bot");
+
+        // Run Seq.2
+        await theClient.LoginAsync(TokenType.Bot, TOKEN);
+        await theClient.StartAsync();
+
+        var _interactionService = new InteractionService(theClient.Rest);
+
+        // Run Seq.3
+        theClient.Ready += async () =>
+        {
+            Console.WriteLine($"Bot demarré, {DateTime.Now}");
+
+            _interactionService = new InteractionService(theClient);
+            await _interactionService.AddModulesAsync(Assembly.GetEntryAssembly(), _serviceProvider);
+            await _interactionService.RegisterCommandsToGuildAsync(1031636775011102720);
+
+            theClient.InteractionCreated += async interaction =>
+            {
+                var scope = _serviceProvider.CreateScope();
+                var ctx = new SocketInteractionContext(theClient, interaction);
+                await _interactionService.ExecuteCommandAsync(ctx, scope.ServiceProvider);
+            };
+
+            StatutRunner();
+        };
+
+        await Task.Delay(-1);
+    }
+
+    public async void StatutRunner()
+    {
+        do
+        {
+            await theClient.SetGameAsync("Salade, tomate, onion ?");
+            Thread.Sleep(5000);
+            await theClient.SetGameAsync("Quelle sauce ?");
+        }
+        while (true);
+    }
+
+    public async void SendLog(string context)
+    {
+        IChannel getLogChan;
+        getLogChan = theClient.GetChannel(1018125885888540732);
+
+        ITextChannel logChan = (ITextChannel)getLogChan;
+
+        await logChan.SendMessageAsync(context);
+    }
+}
